c05ace9342000ce0b8bf13ed5c5fe87e578689f1
aqibau <49211446+aqibau@users.noreply.github.com>
2019-04-02 16:33:45 -0700

    Second commit

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("218794681 - This line was added by Oscar Alcantar.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217756644 - This line was added by Aqib Ullah.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
