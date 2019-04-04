444d2d7812a1885221f12b356cf771d8384335c5
reddys <reddys@SINCLAIR.ecs.csus.edu>
2019-04-04 12:00:39 -0700

    Third commit

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello
 		// Add a new line with your information
 		System.out.println("218794681 - This line was added by Oscar Alcantar.");
 		System.out.println("217756644 - This line was added by Aqib Ullah.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219543156 - This line was added by Suraj Reddy.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
