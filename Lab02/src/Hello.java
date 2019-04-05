80c02b721ea1e08b4bbd3842192b1dbead0e8ad0
Tan Nguyen <Tan Nguyen@10.113.149.19>
2019-04-04 19:13:34 -0700

    Hi, I updated line 11 to my name with my SacStateID

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello
 		System.out.println("218794681 - This line was added by Oscar Alcantar.");
 		System.out.println("217756644 - This line was added by Aqib Ullah.");
 		System.out.println("219543156 - This line was added by Suraj Reddy.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219741289 - This line was added by Tan Nguyen the Great.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
